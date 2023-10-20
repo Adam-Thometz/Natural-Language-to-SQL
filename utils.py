5ad35c1d3e2765b03738c7d93bc94959af2ac1cb
Adam Thometz <adamthometz@gmail.com>
2023-10-20 10:07:11 -0400

    refactor to use chat completion

--- utils.py
+++ utils.py
@@ -1,32 +1,37 @@
 import openai
 
 def create_table_definition(df):
-    prompt = f"""
+    definition = f"""
     ### sqlite SQL table, with its properties:
     # 
     # instruments({",".join(str(col) for col in df.columns)})
     """
 
-    return prompt
+    return definition
 
 def create_prompt(df, query_prompt):
     definition = create_table_definition(df)
     query_init_string = f"### A query to answer: {query_prompt}\nSELECT"
-    return definition + query_init_string
+    messages = [
+        {"role": "system", "content": "You create SQL queries based on given table information and a user prompt"},
+        {"role": "user", "content": definition + query_init_string}
+    ]
+    return messages
 
 def call_openai(query_result):
-    response = openai.Completion.create(
-        model='text-davinci-002',
-        prompt=query_result,
+    response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=query_result,
         temperature=0,
         max_tokens=150,
         top_p=1.0,
         frequency_penalty=0,
         presence_penalty=0,
         stop=['#', ';']
-)
-    query = response['choices'][0]['text']
-    if query.startswith(" "):
+    )
+
+    query = response['choices'][0]['message']['content']
+    if not query.startswith("SELECT"):
         query = "SELECT"+query
     return query
 
